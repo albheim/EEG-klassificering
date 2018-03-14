16f86a13340ee5e5d01f24af861c2777cdc352f7
Albin Heimerson <albheim@hotmail.com>
2018-03-14 16:16:39 +0100

    eeg fixed, try on lunarc

--- lstm_test/eegnet.py
+++ lstm_test/eegnet.py
@@ -1,71 +1,61 @@
-import sys
-from datetime import datetime
+import util
+import data
 
 import numpy as np
-from scipy import io
 
-import tensorflow as tf
-
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Input, GaussianNoise, BatchNormalization
-from keras.layers import TimeDistributed, Lambda, AlphaDropout
+from keras.models import Model
+from keras.layers import Dense, Dropout, Input, BatchNormalization
 from keras.layers import AveragePooling2D, SeparableConv2D
-from keras.layers import Conv2D, MaxPooling2D, Flatten
-from keras.layers import ELU, PReLU, Activation, Reshape
+from keras.layers import Conv2D, Flatten
+from keras.layers import ELU, Reshape
 
-from keras.optimizers import SGD, Adam, RMSprop, Nadam
 from keras import backend as K
-
-from layers import GaussianNoiseAlways
 
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
 
-import data
-import util
-
 
 x, y = data.load_single(cut=True, visual=True, transpose=True)
 xt, yt = data.load_single(cut=True, visual=True, study=False, transpose=True)
-print(x[0].shape)
+print(x[0].shape, xt[0].shape)
 
 splits = 10
 n_subs = len(x)
-n_models = 100
+n_models = 3
 n_evaliter = 10
 msets = [None for j in range(n_models)]
 accs = [0 for j in range(n_models)]
 accs2 = [0 for j in range(n_models)]
 
 
-
 for j in range(n_models):
 
     T, C = x[0][0].shape
-    F = 10
+    F = 8
     N = 3
 
     m_in = Input(shape=(T, C))
-    m_t = Reshape((1, T, C))(m_in)
+    m_t = Reshape((T, C, 1))(m_in)
 
-    m_t = Conv2D(F, (64, 1), padding='same')(m_t)
+    m_t = Conv2D(F, (256, 1), padding='same')(m_t)
     m_t = BatchNormalization()(m_t)
     m_t = SeparableConv2D(F, (1, C), padding='valid')(m_t)
     m_t = BatchNormalization()(m_t)
     m_t = ELU()(m_t)
-    m_t = Dropout(0.2, noise_shape=(None, F, 1, 1))(m_t)
+    m_t = Dropout(0.25, noise_shape=(1, 1, F))(m_t)
 
-    m_t = SeparableConv2D(F, (8, 1), padding='valid')(m_t)
+    m_t = SeparableConv2D(F, (8, 1), padding='same')(m_t)
     m_t = BatchNormalization()(m_t)
     m_t = ELU()(m_t)
     m_t = AveragePooling2D((4, 1))(m_t)
-    m_t = Dropout(0.2, noise_shape=(None, F, 1, 1))(m_t)
+    m_t = Dropout(0.25, noise_shape=(1, 1, F))(m_t)
 
-    m_t = SeparableConv2D(F, (8, 1), padding='valid')(m_t)
+    m_t = SeparableConv2D(2 * F, (8, 1), padding='same')(m_t)
     m_t = BatchNormalization()(m_t)
     m_t = ELU()(m_t)
     m_t = AveragePooling2D((4, 1))(m_t)
-    m_t = Dropout(0.2, noise_shape=(None, F, 1, 1))(m_t)
+    m_t = Dropout(0.25, noise_shape=(1, 1, 2 * F))(m_t)
+
 
     m_t = Flatten()(m_t)
     m_out = Dense(3, activation='softmax')(m_t)
@@ -73,7 +63,7 @@ for j in range(n_models):
     model = Model(inputs=m_in, outputs=m_out)
 
     model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
+                  optimizer='adam',
                   metrics=['accuracy'])
 
     if j == 0:
@@ -93,7 +83,7 @@ for j in range(n_models):
 
             # fit with next kfold data
             h = model.fit(x[i][tr], y[i][tr],
-                          batch_size=64, epochs=50, verbose=0)
+                          batch_size=64, epochs=500, verbose=0)
             h = h.history
 
 
