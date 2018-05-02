31099d8233b126a4015ced0db4cf1c10e73a8fa8
damirbk <damir.basic.knezevic1@gmail.com>
2018-05-02 09:52:33 +0200

    Added some more classifiers to test_csp

--- python/test_csp.py
+++ python/test_csp.py
@@ -28,6 +28,8 @@ import matplotlib.pyplot as plt
 
 from sklearn.pipeline import Pipeline
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.svm import SVC
 from sklearn.model_selection import ShuffleSplit, cross_val_score
 
 from mne import create_info
@@ -47,7 +49,7 @@ tmin, tmax = -1.5, 2.5
 event_id = dict(FA=0, LM=1, OB=2)
 subject = 5
 
-x, y = data.load_single_sub(subject, cut=False, shuffle=False)
+x, y = data.load_single_sub(subject, cut=False, user="Damir")
 x_t = x[:, :, 768:1280]
 y = np.where(y==1)[1]
 print(x.shape, x_t.shape, y.shape)
@@ -74,10 +76,13 @@ cv_split = cv.split(epochs_data_train)
 
 # Assemble a classifier
 lda = LinearDiscriminantAnalysis()
+qda = QuadraticDiscriminantAnalysis()
+svc = SVC()
 csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
 
 # Use scikit-learn Pipeline with cross_val_score function
-clf = Pipeline([('CSP', csp), ('LDA', lda)])
+clf = Pipeline([('CSP', csp), ('SVC', svc)])
+clf.set_params(CSP__reg=0.5)
 scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=1)
 
 # Printing the results
@@ -100,39 +105,39 @@ csp.plot_patterns(info, layout=layout, ch_type='eeg',
 
 ###############################################################################
 # Look at performance over time
-
-sfreq = info['sfreq']
-w_length = int(sfreq * 0.5)   # running classifier: window length
-w_step = int(sfreq * 0.1)  # running classifier: window step size
-w_start = np.arange(0, epochs_data.shape[2] - w_length, w_step)
-
-scores_windows = []
-
-for train_idx, test_idx in cv_split:
-    y_train, y_test = labels[train_idx], labels[test_idx]
-
-    X_train = csp.fit_transform(epochs_data_train[train_idx], y_train)
-    X_test = csp.transform(epochs_data_train[test_idx])
-
-    # fit classifier
-    lda.fit(X_train, y_train)
-
-    # running classifier: test classifier on sliding window
-    score_this_window = []
-    for n in w_start:
-        X_test = csp.transform(epochs_data[test_idx][:, :, n:(n + w_length)])
-        score_this_window.append(lda.score(X_test, y_test))
-    scores_windows.append(score_this_window)
-
-# Plot scores over time
-w_times = (w_start + w_length / 2.) / sfreq + tmin
-
-plt.figure()
-plt.plot(w_times, np.mean(scores_windows, 0), label='Score')
-plt.axvline(0, linestyle='--', color='k', label='Onset')
-plt.axhline(class_balance, linestyle='-', color='k', label='Chance')
-plt.xlabel('time (s)')
-plt.ylabel('classification accuracy')
-plt.title('Classification score over time')
-plt.legend(loc='lower right')
-plt.show()
+#
+#sfreq = info['sfreq']
+#w_length = int(sfreq * 0.5)   # running classifier: window length
+#w_step = int(sfreq * 0.1)  # running classifier: window step size
+#w_start = np.arange(0, epochs_data.shape[2] - w_length, w_step)
+#
+#scores_windows = []
+#
+#for train_idx, test_idx in cv_split:
+#    y_train, y_test = labels[train_idx], labels[test_idx]
+#
+#    X_train = csp.fit_transform(epochs_data_train[train_idx], y_train)
+#    X_test = csp.transform(epochs_data_train[test_idx])
+#
+#    # fit classifier
+#    lda.fit(X_train, y_train)
+#
+#    # running classifier: test classifier on sliding window
+#    score_this_window = []
+#    for n in w_start:
+#        X_test = csp.transform(epochs_data[test_idx][:, :, n:(n + w_length)])
+#        score_this_window.append(lda.score(X_test, y_test))
+#    scores_windows.append(score_this_window)
+#
+## Plot scores over time
+#w_times = (w_start + w_length / 2.) / sfreq + tmin
+#
+#plt.figure()
+#plt.plot(w_times, np.mean(scores_windows, 0), label='Score')
+#plt.axvline(0, linestyle='--', color='k', label='Onset')
+#plt.axhline(class_balance, linestyle='-', color='k', label='Chance')
+#plt.xlabel('time (s)')
+#plt.ylabel('classification accuracy')
+#plt.title('Classification score over time')
+#plt.legend(loc='lower right')
+#plt.show()
